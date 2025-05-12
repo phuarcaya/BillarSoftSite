07ffd3e57cdf2a04499d266a6f915b164f72ebaa
Huarcaya <phuarcaya@gmail.com>
2025-05-12 15:40:03 -0500

    New Number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     'https://www.youtube.com/playlist?list=PLRZbgFkIzQfRF-B4RMe-NAnnu0CWxWF8k',
   urlFacebookPage: 'https://www.facebook.com/billarsoft',
   urlInstagramPage: 'https://www.facebook.com/billarsoft',
-  phoneNumber: '(+51) 954 138 326',
+  phoneNumber: '(+51) 910 209 061',
   emailContact: 'devnet.ph.mkt@gmail.com',
-  urlWhatsApp: 'https://wa.me/51954138326',
+  urlWhatsApp: 'https://wa.me/51910209061',
 };
